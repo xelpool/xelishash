5b9292f7c5b588c33523833c13ff2c64aefa9476
duggavo <duggavo@proton.me>
2025-01-07 18:55:39 +0100

    add another unit test to be sure

--- xelishashv2_test.go
+++ xelishashv2_test.go
@@ -47,10 +47,9 @@ func TestVerifyOutput(t *testing.T) {
 		89, 50, 197, 115, 231, 32, 74, 2, 92, 47, 25, 220, 135, 249, 122,
 		172, 220, 137, 143, 234, 68, 188,
 	}
+	input2 := []byte{83, 175, 21, 164, 59, 64, 112, 22, 133, 157, 110, 93, 103, 233, 95, 171, 84, 212, 94, 159, 56, 231, 142, 83, 155, 90, 210, 84, 73, 195, 107, 38, 0, 0, 1, 148, 65, 210, 149, 206, 0, 0, 0, 0, 0, 0, 2, 111, 30, 180, 107, 152, 2, 158, 60, 146, 72, 97, 3, 240, 133, 110, 18, 13, 196, 213, 137, 255, 172, 43, 178, 237, 0, 0, 0, 0, 0, 0, 0, 1, 80, 105, 173, 140, 96, 184, 216, 33, 205, 190, 44, 59, 87, 223, 214, 64, 226, 151, 200, 115, 89, 42, 131, 251, 182, 18, 47, 210, 108, 219, 69, 126}
 
 	scratchpad := ScratchPadV2{}
-
-	hash := XelisHashV2(input, &scratchpad)
 
 	expectedHash := [32]byte{
 		199, 114, 154, 28, 4, 164, 196, 178, 117, 17, 148,
@@ -58,9 +57,18 @@ func TestVerifyOutput(t *testing.T) {
 		55, 244, 178, 94, 29, 248, 242, 98, 221, 158, 179,
 	}
 
+	expectedHash2 := [32]byte{86, 153, 158, 47, 177, 49, 55, 60, 155, 61, 147, 124, 179, 204, 11, 76, 59, 90, 186, 134, 9, 20, 21, 248, 156, 47, 122, 116, 118, 227, 24, 75}
+
+	hash := XelisHashV2(input, &scratchpad)
 	if hash != expectedHash {
 		t.Fatalf("incorrect hash: %x, expected: %x", hash, expectedHash)
 	}
+
+	hash = XelisHashV2(input2, &scratchpad)
+	if hash != expectedHash2 {
+		t.Fatalf("incorrect hash: %x, expected: %x", hash, expectedHash)
+	}
+	t.Logf("%x", hash)
 }
 
 func BenchmarkHashV2(b *testing.B) {
